445b7bb8a8344f97a81830d7c3146ff01689e734
Jason Zaitz <jasonzaitz1@gmail.com>
2019-01-07 11:28:18 -0500

    added challenge

--- CodeChallenge.rb
+++ CodeChallenge.rb
@@ -434,3 +434,18 @@ def to_underscore(string)
   return ans
 
 end
+
+#Sheldon, Leonard, Penny, Rajesh and Howard are in the queue for a "Double Cola" drink vending machine; there are no other people in the queue. The first one in the queue (Sheldon) buys a can, drinks it and doubles! The resulting two Sheldons go to the end of the queue. Then the next in the queue (Leonard) buys a can, drinks it and gets to the end of the queue as two Leonards, and so on.
+#Write a program that will return the name of the person who will drink the n-th cola.
+def whoIsNext(names, r)
+  # your code
+  names = ["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"]
+
+  r.times do |num|
+    x = names[0]
+    names = names[1..-1]
+    2.times {names.push(x)}
+  end
+
+  return names[-1]
+end
